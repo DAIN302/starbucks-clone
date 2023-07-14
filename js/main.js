81140e6b7a6b5713bc2eaaca008793bf7396596a
DAIN302 <dbgnfk12@gmail.com>
2023-07-14 15:54:00 +0900

    메인 컨텐츠 영역
    비쥬얼 영역 - gsap.to()를 사용해서 요소가 순차적으로 나오게 구현
    공지사항 - swiper 를 사용해서 일정시간마다 공지사항 컨텐츠가 슬라이드 되게 구현(direction - vertical)
    이미지 슬라이드 영역 -
    swiper를 사용하여 슬라이드 기능 구현, 슬라이드 토글 기능 구현

--- js/main.js
+++ js/main.js
@@ -43,5 +43,60 @@ window.addEventListener("DOMContentLoaded",()=>{
     // _.throttle(함수, 시간)
     // 스크롤이 될때마다 함수가 호출되면 부하가 오므로 이를 방지하기 위한 라이브러리
 
+    // 비쥬얼
+    const fadeEls = document.querySelectorAll(".visual .fade-in");
+    fadeEls.forEach(function(fadeEl, index){
+        gsap.to(fadeEl, 1, {
+            delay : (index+1) * .7, // delay라는 옵션으로 지연시간 추가, 앞에 곱하기를 써서 요소가 순차적으로 나타나게함
+            opacity : 1
+        })
+    });
 
-})
+    // 공지사항 - swiper
+    // new Swiper(선택자, 옵션)
+    new Swiper('.notice-line .swiper-container', {
+        direction : 'vertical',
+        autoplay : true,
+        loop : true
+    });
+
+    // 이미지 슬라이드 - swiper
+    new Swiper('.promotion .swiper-container', {
+        slidesPerView : 3, 
+        spaceBetween : 10,
+        centeredSlides : true,
+        loop : true,
+        autoplay : {
+            delay : 5000
+        },
+        pagination : {
+            el : '.promotion .swiper-pagination',
+            clickable : true
+        },
+        navigation : {
+            prevEl : '.promotion .swiper-prev',
+            nextEl : '.promotion .swiper-next'
+        }
+    })
+
+    // 슬라이드 토글 기능
+    const promotionEl = document.querySelector(".promotion")
+    const promotionToggleBtn = document.querySelector('.toggle-promotion')
+    let isHidePromotion = false;
+
+    promotionToggleBtn.addEventListener('click', function(){
+        // 클릭 시 반대값으로 전환
+        isHidePromotion = !isHidePromotion
+
+        if(isHidePromotion) {
+            // 숨기기
+            promotionEl.classList.add('hide');
+        }
+        else {
+            // 보이기
+            promotionEl.classList.remove('hide');
+        }
+    })
+
+
+}) // 로드구역
